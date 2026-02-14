db006b3cc300e164912a54ed8e3b3fd7febeac20
Chris9699 <41058826+Chris9699@users.noreply.github.com>
2026-02-15 00:35:19 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,5 +1,7 @@
 const { Client, GatewayIntentBits } = require('discord.js');
 
+const PING_CHANNEL = process.env.PING_CHANNEL || 'pings';
+
 const client = new Client({
   intents: [
     GatewayIntentBits.Guilds,
@@ -8,14 +10,16 @@ const client = new Client({
   ]
 });
 
-client.on('ready', () => {
+client.on('clientReady', () => {
   console.log(`Bot logged in as ${client.user.tag}`);
 });
 
 client.on('messageCreate', (message) => {
   if (message.author.bot) return;
   
-  console.log(`[${message.author.username}] ${message.content}`);
+  if (message.channel.name === PING_CHANNEL) {
+    console.log(`[${message.author.username}] ${message.content}`);
+  }
 });
 
 client.login(process.env.DISCORD_TOKEN);
